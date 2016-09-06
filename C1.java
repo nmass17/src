5c8ba5147289222113c3a087c8fe224af1d77007
nmass17 <windward_student@school.windwardschool.org>
2016-09-06 08:49:03 -0700

    Method Practice

---
+++ C1.java
@@ -0,0 +1,17 @@
+
+public class C1
+{
+	public static void main(String[] args)
+	{
+		drawbox();
+		drawbox();
+		drawbox();
+	}
+	public static void drawbox()
+	{
+		System.out.println("+------+");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("+------+\n");
+	}
+}
